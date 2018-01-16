d882831fe39445e17c300eb9f3eb38cb6ce8a859
hb-2017 <819920696@qq.com>
2018-01-16 18:22:18 +0800

    修改节点账号

--- run_case/All_node_login.py
+++ run_case/All_node_login.py
@@ -18,8 +18,10 @@ suite.addTest(Load_drive("test_quit_browser"))  #退出浏览器
 if __name__=='__main__':
     # 实例化测试报告
     re = report()
-    fp = re.report_exist_statu()
+    report_title = 'All_node_login'
+    fp = re.report_exist_statu(report_title)
     # 初始化一个HTMLTestRunner实例对象，用来生成报告
     runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title=u"全节点登录测试", description=u"用例测试情况")
     # 开始执行测试套件
     runner.run(suite)
+    fp.close()
